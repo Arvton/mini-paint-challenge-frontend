2998a454897317385923c82c98a656866f2b0972
Arvin Josol <arvin.josol@live.com>
2024-01-27 12:49:48 -0800

    add hero component and move hero html: - move hero scetion to hero component - remove global styles which will be handled at component level

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
+import Hero from './components/Hero/Hero'
 import './App.scss'
 
-function App() {
+export default function App() {
 
   return (
     <>
-      <p>Hero image placeholder</p>
-      <h2>Shrink the Canvas. Expand Your Imagination.</h2>
-      <div className="card">
-        <p>Learn to oil paint with me during the Mini Paint Challenge. I host a free weekly
-          lesson live on YouTube.</p>
-        <button>Join the challenge</button>
-      </div>
+      <Hero />
     </>
   )
 }
-
-export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import "./Hero.scss";
+
+export default function Hero() {
+    return (
+        <section className="hero-section">
+            <div className="hero__mobile">
+                <p>Hero image placeholder</p>
+                <h2>Shrink the Canvas. Expand Your Imagination.</h2>
+                <div className="card">
+                    <p>Learn to oil paint with me during the Mini Paint Challenge. I host a free weekly
+                        lesson live on YouTube.</p>
+                    <button>Join the challenge</button>
+                </div>
+            </div>
+        </section>
+    )
+}
