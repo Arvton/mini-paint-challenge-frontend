2a353b48b7ac5cddcef180757510ec6c9adadbda
Arvin Josol <arvin.josol@live.com>
2024-01-27 12:18:23 -0800

    Remove boilerplate. Add scss partials, start files - install sass - add global style reset - add fonts:     - Archivo, Archivo Black, Archivo Narrow - add variables:     - colors from Megan Josol Art branding     - breakpoints - add mixins:     - media queries for tablet and desktop     - spacing - add typography mixins - change site title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Mini Paint Challenge</h1>
+      <p>Hero image placeholder</p>
+      <h2>Shrink the Canvas. Expand Your Imagination.</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Placeholder code for the Mini Paint Challenge
-        </p>
+        <p>Learn to oil paint with me during the Mini Paint Challenge. I host a free weekly
+          lesson live on YouTube.</p>
+        <button>Join the challenge</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
