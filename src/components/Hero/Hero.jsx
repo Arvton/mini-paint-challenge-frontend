6d779f3f30857bff07301a7e39d8fc505e35f466
Arvin Josol <arvin.josol@live.com>
2024-01-29 16:48:54 -0800

    fix hero header sizing. add bg color to App

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,9 @@ export default function Hero() {
             <div className="hero__mobile">
                 <img className="hero__mobile-img" src={HeroImg} alt="Mini Paint Challenge hero image"></img>
                 <div className="hero__mobile-container">
-                    <h2 className="hero__mobile-header">{`"Shrink the canvas. Expand your imagination."`}</h2>
+                    <h2 className="hero__mobile-header">Shrink the canvas. Expand your imagination.</h2>
                     <p className="hero__mobile-details">Create a mini piece of art with me. I will show
-                        you how during the Mini Paint Challenge; a free weekly livestream on YouTube.</p>
+                        you how on the Mini Paint Challenge, a free weekly YouTube livestream.</p>
                     <button className="hero__mobile-cta">Join the challenge</button>
                 </div>
             </div>
