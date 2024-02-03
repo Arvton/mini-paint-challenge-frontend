736b9e4d3d067abc95ec77e193ad2d1a61af45c3
Arvin Josol <arvin.josol@live.com>
2024-02-03 10:14:57 -0800

    add responsive style to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,8 +8,10 @@ export default function Hero({ onClick }) {
         <section className="hero">
             <img className="hero__img" src={HeroImg} alt="Mini Paint Challenge hero image"></img>
             <div className="hero__container">
-                <h2 className="hero__header">Shrink the canvas. Expand your imagination.</h2>
-                <p className="hero__details">Explore the world of mini paintings every week during my free LIVE painting lessons.</p>
+                <div className="hero__text-container">
+                    <h2 className="hero__header">Shrink the canvas. Expand your imagination.</h2>
+                    <p className="hero__details">Explore the world of mini paintings every week during my free LIVE painting lessons.</p>
+                </div>
                 <ButtonCTA className="hero__cta" content="Join the Challenge" onClick={onClick} />
             </div>
         </section>
